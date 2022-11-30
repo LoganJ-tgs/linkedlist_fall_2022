00acb8ad1251ec95479d4cf1823e757c5c012623
logan Judge <115499873+LoganJ-tgs@users.noreply.github.com>
2022-11-30 19:58:09 +0000

    sorting functionalities

--- main.py
+++ main.py
@@ -5,6 +5,45 @@ class LinkedList:
     # are not recognizable outside the class when two underscores
     # precede a name but aren't followed by two underscores at the
     # end of the name (i.e. an operator name).
+    def getitematindex(self, index):
+      Prior = None
+      cursor = self.first.getNext()
+      for i in range(index):
+        Prior = cursor
+        cursor = cursor.getNext()
+      return cursor, Prior
+
+    def append(self, item):
+        node = LinkedList.__Node(item)
+        self.last.setNext(node)
+        self.last = node
+        self.numItems += 1
+
+    def isSorted(self):
+      cursor = self.first.getNext()
+      prioritem = cursor.getItem()
+      while cursor is not None:
+        if cursor.getItem() < prioritem:
+          return False
+        prioritem = cursor.getItem()
+        cursor = cursor.getNext()
+      return True
+    
+    def swap(self, index1, index2):
+      node1, n = self.getitematindex(index1)
+      node2, n = self.getitematindex(index2)
+      item1saved = node1.getItem()
+      node1.setItem(node2.getItem())
+      node2.setItem(item1saved)
+
+    def bubblesort(self):
+      length = len(self)
+      while length > 0:
+        for x in range(0, length - 1):
+          if self[x] > self[x + 1]:
+            self.swap(x, x+1)
+        length -= 1
+    
     class __Node:
         def __init__(self, item, next=None):
             self.item = item
@@ -57,45 +96,96 @@ class LinkedList:
         raise IndexError("LinkedList assignment index out of range")
 
     def insert(self, index, item):
-        # This is left as an exercise for the reader.
-        pass
+      if index == 0:
+        newnode = LinkedList.__Node(item, self.first.getNext())
+        self.first.setNext(newnode)
+        self.numItems += 1
+        return
+      
+      if 1 <= index:
+        cursor = self.first.getNext()
+        
+        # get the item and at index
+        for x in range(1, index, 1):
+          cursor = cursor.getNext()
+          if cursor == None:
+            cursor = self.first.getNext()
 
+        #set next of cursor to newnode
+        #set next of newnode to next of cursor old
+        newnode = LinkedList.__Node(item, cursor.getNext())
+        cursor.setNext(newnode)
+        self.numItems += 1
+        
+        return
+      raise IndexError("LinkedList assignment index out of range")
+
+    def __contains__(self, item):
+      cursor = self.first.getNext()
+      while cursor is not None:
+        if cursor.getItem() == item:
+          return True
+        cursor = cursor.getNext()
+      return False
+    
     def __add__(self, other):
         if type(self) != type(other):
             raise TypeError("Concatenate undefined for " + \
                             str(type(self)) + " + " + str(type(other)))
 
         result = LinkedList()
+        cursorself = self.first.getNext()
+        while cursorself is not None:
+          result.append(cursorself.getItem())
+          cursorself = cursorself.getNext()
 
-        #TODO: Finish this.
+        cursorother = other.first.getNext()
+        while cursorother is not None:
+          result.append(cursorother.getItem())
+          cursorother = cursorother.getNext()
 
         return result
 
-    def __contains__(self, item):
-        # This is left as an exercise for the reader.
-        pass
+    
 
     def __delitem__(self, index):
-        # This is left as an exercise for the reader.
-        pass
+        item, prior = self.getitematindex(index)
+        next = item.getNext()
+        prior.setNext(next)
+        self.numItems -= 1
 
     def __eq__(self, other):
-        # This is left as an exercise for the reader.
-        pass
+      if len(self) != len(other) or type(self) != type(other):
+        return False
+
+      curself = self.first.getNext()
+      curother = other.first.getNext()
+
+      while curself is not None:
+        if curself.getItem() != curother.getItem():
+          return False
+
+        curself = curself.getNext()
+        curother = curother.getNext()
+      return True
+      
 
     def __len__(self):
-        # This is left as an exercise for the reader.
-        pass
+      return self.numItems
 
-    def append(self, item):
-        node = LinkedList.__Node(item)
-        self.last.setNext(node)
-        self.last = node
-        self.numItems += 1
+    
 
     def __str__(self):
-        # This is left as an exercise for the reader.
-        pass
+      cursor = self.first.getNext()
+      outString = "["
+
+      while cursor is not None:
+        outString += str(cursor.getItem()) + ","
+        cursor = cursor.getNext()
+
+      outString = outString.rstrip(",")
+      outString += "]"
+      return outString
 
 
 def main():
@@ -103,11 +193,7 @@ def main():
 
     for i in range(100):
         lst.append(i)
-
     lst2 = LinkedList(lst)
-
-    print(lst)
-    print(lst2)
 
     if lst == lst2:
         print("Test 1 Passed")
@@ -159,6 +245,9 @@ def main():
     lst.insert(0, 100)
     lst4 = LinkedList([100]) + lst4
 
+    print(lst)
+    print(lst4)
+
     if lst == lst4:
         print("Test 9 Passed")
     else:
@@ -167,14 +256,17 @@ def main():
     lst.insert(1000, 333)
     lst4.append(333)
 
+
     if lst == lst4:
         print("Test 10 Passed")
     else:
         print("Test 10 Failed")
 
-    print(lst)
-    print(lst4)
-
+    
+def main2():
+  lst = LinkedList([2, 3, 8, 1, 21, 23, 21, 5, 5, 5, 233, 932, 902838289392, 0, 8, 10, 243, 213])
+  lst.bubblesort()
+  print(lst)
 
 if __name__ == "__main__":
-    main()
+    main2()
